9c8a03087891ca5cfbe6ebd4bd7dd73302ff7a4c
lessouffrances <dong.pa@northeastern.edu>
2026-04-04 18:47:36 -0400

    configure Jackson+XML accept format

--- src/main/java/com/example/mobileappws/ui/controller/UserController.java
+++ src/main/java/com/example/mobileappws/ui/controller/UserController.java
@@ -7,7 +7,10 @@ import com.example.mobileappws.ui.model.response.UserRest;
 import org.apache.catalina.User;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
@@ -17,7 +20,7 @@ public class UserController {
     UserService userService;
 
     // http://localhost:8080/users/o9vMepZFDxGW7DzgLSS24jHWtBo9ir
-    @GetMapping(path="/{id}")
+    @GetMapping(path="/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
     public UserRest getUser(@PathVariable String id)
     {
         UserRest returnValue = new UserRest();
