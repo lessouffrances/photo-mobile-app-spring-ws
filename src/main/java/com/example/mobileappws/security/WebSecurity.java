d8c9bc1db2d96bc599ac1d28ae714453a7cca8e9
lessouffrances <dong.pa@northeastern.edu>
2026-05-11 14:05:59 -0400

    configure the password-reset endpoint to be public

--- src/main/java/com/example/mobileappws/security/WebSecurity.java
+++ src/main/java/com/example/mobileappws/security/WebSecurity.java
@@ -55,6 +55,7 @@ public class WebSecurity {
                 .requestMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
                 .requestMatchers(HttpMethod.GET, SecurityConstants.VERIFICATION_EMAIL_URL).permitAll()
                 .requestMatchers(HttpMethod.POST, SecurityConstants.PASSWORD_RESET_REQUEST_URL).permitAll()
+                .requestMatchers(HttpMethod.POST, SecurityConstants.PASSWORD_RESET_URL).permitAll()
                 .anyRequest().authenticated()
             )
             .authenticationManager(authenticationManager)
